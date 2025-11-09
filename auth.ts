0ee3ba3b2ebcef1c3ea1bf95329cbd9578be09ee
Savze7 <didaattalah@gmail.com>
2025-11-09 22:29:49 +0700

    Rolebase_done

--- auth.ts
+++ auth.ts
@@ -1,12 +1,24 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import { prisma } from "./lib/prisma";
+import NextAuth from "next-auth"
+import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
  
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
-  session: { strategy: "jwt" },
-  pages: { signIn: "/signin" },
-})
+  session:{ strategy:"jwt"},
+  pages:{
+    signIn: "/signin",
+  },
+  callbacks:{
+    jwt({token, user}){
+      if(user) token.role = user.role;
+      return token;
+    },
+    session({session, token}){
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      return session;
+    }
+  }
+});
