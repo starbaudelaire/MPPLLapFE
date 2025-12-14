a43212c50837a95912bbec07e92e036ee40a2db8
starbaudelaire <inibintang.alka@gmail.com>
2025-12-14 21:43:44 +0700

    Chore: Fix generated prisma client path

--- app/generated/prisma/internal/class.ts
+++ app/generated/prisma/internal/class.ts
@@ -23,11 +23,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-<<<<<<< HEAD
       "value": "D:\\mppl\\mergebos\\MPPLLapFE\\app\\generated\\prisma",
-=======
-      "value": "D:\\MPPL DISINI MAL\\MPPLLapFE\\app\\generated\\prisma",
->>>>>>> origin/feature/admin-dashboard
       "fromEnvVar": null
     },
     "config": {
@@ -41,11 +37,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-<<<<<<< HEAD
     "sourceFilePath": "D:\\mppl\\mergebos\\MPPLLapFE\\prisma\\schema.prisma",
-=======
-    "sourceFilePath": "D:\\MPPL DISINI MAL\\MPPLLapFE\\prisma\\schema.prisma",
->>>>>>> origin/feature/admin-dashboard
     "isCustomOutput": true
   },
   "relativePath": "../../../prisma",
